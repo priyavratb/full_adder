899b872becf5735c6878c6578e2adf5bc2f67952
priyavratb <56146190+priyavratb@users.noreply.github.com>
2019-11-27 05:01:13 -0500

    Add files via upload

---
+++ fa_nbit.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/03/2019 09:33:11 PM
+// Design Name: 
+// Module Name: fa_nbit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fa_nbit(a,b,cin,sum,cout);
+
+input [n-1:0] a,b;
+input cin;
+output sum, cout;
+endmodule
